9b9a984f61b099baf30048d7a9cf62fc1bc7ec3b
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-02-22 21:03:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ for i in info[0,:]:
     cv2.circle(gray,(i[0],i[1]),i[2],(255,0,0),2)
     cv2.circle(gray,(i[0],i[1]),2,(0,0,255),3)
 
+img =gray
+x_2 = int((img.shape[1]) * ((math.sqrt(230)/8.5)))
+resized2_ = cv2.resize(img,(x_2, img.shape[0]))
 
 
 """
@@ -40,6 +43,6 @@ img = cv2.imread('hello.jpg',0)
 """
 
 
-cv2.imshow('oval', gray)
+cv2.imshow('oval', resized2_)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
