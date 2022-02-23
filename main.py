535de776a744962c38475acf9aaa66d00f29b566
Pl251251 <91552726+Pl251251@users.noreply.github.com>
2022-02-22 21:36:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,25 +24,25 @@ circles = cv2.HoughCircles(blur,cv2.HOUGH_GRADIENT,1,20,
 info = np.uint16(np.around(circles))
 a=0
 for i in info[0,:]:
-    cv2.circle(gray,(i[0],i[1]),i[2],(255,0,0),2)
-    cv2.circle(gray,(i[0],i[1]),2,(0,0,255),3)
+    cv2.circle(resized_,(i[0],i[1]),i[2],(255,0,0),2)
+    cv2.circle(resized_,(i[0],i[1]),2,(0,0,255),3)
 
-img =gray
+img =resized_
 x_2 = int((img.shape[1]) * ((math.sqrt(230)/8.5)))
 resized2_ = cv2.resize(img,(x_2, img.shape[0]))
 
 
-"""
-cv2.imwrite("hello4.jpg", gray)
+
+cv2.imwrite("hello4.jpg", resized2_)
 #pillow
 im = Image.open('hello4.jpg')
 im_rotate = im.rotate(12)
 im = im_rotate.save("hello.jpg")
 #opencv
 img = cv2.imread('hello.jpg',0)
-"""
 
 
-cv2.imshow('oval', resized2_)
+
+cv2.imshow('oval', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
